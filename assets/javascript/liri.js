08dcf181bb30d826b0f0cc57b692d1f040a99f1a
Alex C. B <43941153+acbihm@users.noreply.github.com>
2019-02-20 02:04:57 -0600

    Added package.json, dependencies, Javascript assets, .env, and .gitignore

---
+++ assets/javascript/liri.js
@@ -0,0 +1,44 @@
+require("dotenv").config();
+  var keys = require("./keys.js");
+
+
+
+//   9. Make it so liri.js can take in one of the following commands:
+
+//    * `concert-this`
+
+//    * `spotify-this-song`
+
+//    * `movie-this`
+
+//    * `do-what-it-says`
+
+// ### What Each Command Should Do
+
+// 1. `node liri.js concert-this <artist/band name here>`
+
+//    * This will search the Bands in Town Artist Events API (`"https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp"`) for an artist and render the following information about each event to the terminal:
+
+//      * Name of the venue
+
+//      * Venue location
+
+//      * Date of the Event (use moment to format this as "MM/DD/YYYY")
+
+// 2. `node liri.js spotify-this-song '<song name here>'`
+
+//    * This will show the following information about the song in your terminal/bash window
+
+//      * Artist(s)
+
+//      * The song's name
+
+//      * A preview link of the song from Spotify
+
+//      * The album that the song is from
+
+//    * If no song is provided then your program will default to "The Sign" by Ace of Base.
+
+//    * You will utilize the [node-spotify-api](https://www.npmjs.com/package/node-spotify-api) package in order to retrieve song information from the Spotify API.
+
+//    * The Spotify API requires you sign up as a developer to generate the necessary credentials. You can follow these steps in order to generate a **client id** and **client secret**:
