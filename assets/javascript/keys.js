eab46eabdd7453a852b19d325844df0b04ddc852
Alex C. B <43941153+acbihm@users.noreply.github.com>
2019-02-24 22:05:31 -0600

    Added basic functionality of concert-this, spotify-this-song, and movie-this

--- assets/javascript/keys.js
+++ assets/javascript/keys.js
@@ -1,7 +1,11 @@
-
 console.log('this is loaded');
-
 exports.spotify = {
   id: process.env.SPOTIFY_ID,
   secret: process.env.SPOTIFY_SECRET
 };
+
+// exports.spotify = {
+//   id: 'efb3ce85ce0c4fe7b34f8edd991635e2',
+//   secret: 'cd52a4dc308848afac3e160220ef2dc8'
+// };
+
--- assets/javascript/liri.js
+++ assets/javascript/liri.js
@@ -1,45 +1,95 @@
 require("dotenv").config();
-  var keys = require("./keys.js");
-  var spotify = new Spotify(keys.spotify);
+var keys = require("./keys.js");
+var Spotify = require("node-spotify-api");
+var spotify = new Spotify(keys.spotify);
+var moment = require("moment");
+var fs = require("fs");
+var request = require("request");
+var cTable = require('console.table');
+var axios = require("axios");
 
 
 
-//   9. Make it so liri.js can take in one of the following commands:
+var command = process.argv[2]
+var term = process.argv.slice(3).join(" ");
+if (command === "concert-this") {
+  console.log("Searching for concerts");
+  concertThis(term);
+}
+else if (command === "spotify-this-song") {
+  console.log("Searching Spotify for this song");
+  spotifyThis(term)
+}
+else if (command === "movie-this") {
+  console.log("Searching OMDB for this movie");
+  movieThis(term);
+}
+else if (command === "do-what-it-says") {
+  console.log("One moment...");
+  doThing(term);
+}
+else {
+  console.log("Error");
+}
 
-//    * `concert-this`
+function spotifyThis(term) {
+  spotify.search({ type: 'track', query: term }, function (err, data) {
+    if (err) {
+      return console.log('Error occurred: ' + err);
+    }
+    var searchRes = data.tracks.items;
+    console.log('=====================');
+    console.log('');
+    console.log('Here are your top 3 results for ' + "'"+ term +"'");
+    for (var i = 0; i < 3; i++) {
+      var result = {
+        artist: searchRes[i].album.artists[0].name,
+        album_name: searchRes[i].album.name,
+        song_name: searchRes[i].name,
+        preview_url: searchRes[i].preview_url
+        
+      }
+      console.table(result)
+    }
+  });
+}
 
-//    * `spotify-this-song`
+function concertThis(term) {
 
-//    * `movie-this`
+  axios.get("https://rest.bandsintown.com/artists/" + term + "/events?app_id=codingbootcamp").then(
+    function(response) {
+      var result = response.data;
+      console.log('');
+      console.log('=====================');
+      console.log('');
+      console.log("Here is "  + term + "\'s next live show: " );
+      console.log('');
+      console.log(result[0].datetime);
+      console.log(result[0].venue.name)
+      console.log(result[0].venue.city + ", " + result[0].venue.region);
+      console.log('');
+      console.log('');
+    }
+  );
+}
 
-//    * `do-what-it-says`
 
-// ### What Each Command Should Do
+function movieThis(term) {
+  var queryUrl = "http://www.omdbapi.com/?t=" + term + "&y=&plot=short&apikey=trilogy";
 
-// 1. `node liri.js concert-this <artist/band name here>`
 
-//    * This will search the Bands in Town Artist Events API (`"https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp"`) for an artist and render the following information about each event to the terminal:
+  axios.get(queryUrl).then(
+    function(response) {
+      var result = response.data;
+      console.log('');
+      console.log('=====================');
+      console.log('');
+      console.log("Here is what I found about "  + "'"+term +"'" );
+      console.log('');
+      console.log(result);
 
-//      * Name of the venue
-
-//      * Venue location
-
-//      * Date of the Event (use moment to format this as "MM/DD/YYYY")
-
-// 2. `node liri.js spotify-this-song '<song name here>'`
-
-//    * This will show the following information about the song in your terminal/bash window
-
-//      * Artist(s)
-
-//      * The song's name
-
-//      * A preview link of the song from Spotify
-
-//      * The album that the song is from
-
-//    * If no song is provided then your program will default to "The Sign" by Ace of Base.
-
-//    * You will utilize the [node-spotify-api](https://www.npmjs.com/package/node-spotify-api) package in order to retrieve song information from the Spotify API.
-
-//    * The Spotify API requires you sign up as a developer to generate the necessary credentials. You can follow these steps in order to generate a **client id** and **client secret**:
+      console.log('');
+      console.log('');
+    }
+  );
+}
