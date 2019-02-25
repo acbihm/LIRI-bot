a7e7487cedd9064403f3e93359c81b229d9bedfd
Alex C. B <43941153+acbihm@users.noreply.github.com>
2019-02-24 22:55:31 -0600

    Spotify, concert, movie, do-what-it-says are fully functional. Have not started on Bonus goals yet. Still needs some formatting tweaks to be more cconsistent

--- assets/javascript/liri.js
+++ assets/javascript/liri.js
@@ -1,17 +1,16 @@
 require("dotenv").config();
-var keys = require("./keys.js");
-var Spotify = require("node-spotify-api");
-var spotify = new Spotify(keys.spotify);
-var moment = require("moment");
-var fs = require("fs");
-var request = require("request");
-var cTable = require('console.table');
-var axios = require("axios");
-
-
+const keys = require("./keys.js");
+const Spotify = require("node-spotify-api");
+const spotify = new Spotify(keys.spotify);
+const moment = require("moment");
+const fs = require("fs");
+const request = require("request");
+const cTable = require('console.table');
+const axios = require("axios");
 
 var command = process.argv[2]
 var term = process.argv.slice(3).join(" ");
+
 if (command === "concert-this") {
   console.log("Searching for concerts");
   concertThis(term);
@@ -26,28 +25,31 @@ else if (command === "movie-this") {
 }
 else if (command === "do-what-it-says") {
   console.log("One moment...");
-  doThing(term);
+  doThing();
 }
 else {
   console.log("Error");
 }
 
 function spotifyThis(term) {
-  spotify.search({ type: 'track', query: term }, function (err, data) {
+  if (term == undefined) {
+    songName = "The Sign Ace of Base"
+  }
+  else { var songName = term; }
+  spotify.search({ type: 'track', query: songName }, function (err, data) {
     if (err) {
       return console.log('Error occurred: ' + err);
     }
     var searchRes = data.tracks.items;
     console.log('=====================');
     console.log('');
-    console.log('Here are your top 3 results for ' + "'"+ term +"'");
+    console.log('Here are your top 3 results for ' + "'" + term + "'");
     for (var i = 0; i < 3; i++) {
       var result = {
         artist: searchRes[i].album.artists[0].name,
         album_name: searchRes[i].album.name,
         song_name: searchRes[i].name,
         preview_url: searchRes[i].preview_url
-        
       }
       console.table(result)
     }
@@ -57,12 +59,12 @@ function spotifyThis(term) {
 function concertThis(term) {
 
   axios.get("https://rest.bandsintown.com/artists/" + term + "/events?app_id=codingbootcamp").then(
-    function(response) {
+    function (response) {
       var result = response.data;
       console.log('');
       console.log('=====================');
       console.log('');
-      console.log("Here is "  + term + "\'s next live show: " );
+      console.log("Here is " + term + "\'s next live show: ");
       console.log('');
       console.log(result[0].datetime);
       console.log(result[0].venue.name)
@@ -73,23 +75,39 @@ function concertThis(term) {
   );
 }
 
-
 function movieThis(term) {
-  var queryUrl = "http://www.omdbapi.com/?t=" + term + "&y=&plot=short&apikey=trilogy";
-
-
+  if (term == undefined) {
+    movieName = "Mr. Nobody"
+  }
+  else { var movieName = term; }
+  var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";
   axios.get(queryUrl).then(
-    function(response) {
+    function (response) {
       var result = response.data;
       console.log('');
       console.log('=====================');
       console.log('');
-      console.log("Here is what I found about "  + "'"+term +"'" );
+      console.log(`Here is what I found about ${result.Title}`);
       console.log('');
-      console.log(result);
-
+      console.log(`   ${result.Title} was released in ${result.Year} and starred ${result.Actors}`);
+      console.log(`   It has a rating of ${result.Ratings[0].Value} on ${result.Ratings[0].Source}`);
+      console.log(`   On ${result.Ratings[1].Source}, the film has a rating of ${result.Ratings[1].Value} `);
+      console.log('');
+      console.log(`   The film was produced in on ${result.Country}. The film's audio is in ${result.Language}`)
+      console.log('');
+      console.log(`   The plot of ${result.Title}`)
+      console.log(`  ${result.Plot} `)
       console.log('');
       console.log('');
     }
   );
+}
+
+function doThing(randomThing) {
+  fs.readFile("random.txt", "utf8", function (error, data) {
+    if (error) {
+      return console.log(error);
+    }
+    movieThis(data)
+  });
 }
